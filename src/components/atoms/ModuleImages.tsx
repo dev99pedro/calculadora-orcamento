248de7f6d5a2cb4352b74a5547b73974d5b84897
PMWEB\evandro.guimaraes <evandro.guimaraes@pmweb.com.br>
2025-01-16 15:48:25 -0300

    include new modules infos and structure

---
+++ src/components/atoms/ModuleImages.tsx
@@ -0,0 +1,54 @@
+import image1 from '../assets/images/Image full.jpg';
+import image2 from '../assets/images/2 Imagens Laterais.jpg';
+import image3 from '../assets/images/4 Imagens Sequenciais.jpg';
+import image4 from '../assets/images/2 Imagens.jpg';
+import image5 from '../assets/images/3 Imagens.jpg';
+import image6 from '../assets/images/4 Imagens.jpg';
+import image7 from '../assets/images/5 Imagens.jpg';
+import image8 from '../assets/images/Mosaico 4 Imagens.jpg';
+import image9 from '../assets/images/Título secundário.jpg';
+import image10 from '../assets/images/Texto 1 coluna sem fundo.jpg';
+import image11 from '../assets/images/Texto 1 coluna (fundo em cor).jpg';
+import image12 from '../assets/images/2 Colunas de Texto.jpg';
+import image13 from '../assets/images/Texto com 2 colunas.jpg';
+import image14 from '../assets/images/3 Colunas de Texto.jpg';
+import image15 from '../assets/images/Texto com 3 colunas.jpg';
+import image16 from '../assets/images/Imagem de Fundo + Texto Simples.jpg';
+import image17 from '../assets/images/Imagem de Fundo + Texto Completo.jpg';
+import image18 from '../assets/images/Texto e Imagem à Esquerda.jpg';
+import image19 from '../assets/images/Texto e Imagem à Direita.jpg';
+import image20 from '../assets/images/Texto e 2 Imagens à Esquerda.jpg';
+import image21 from '../assets/images/Texto e 2 Imagens à Direita.jpg';
+import image22 from '../assets/images/2 Colunas de Texto e Imagem.jpg';
+import image23 from '../assets/images/Texto e Imagem em 2 Colunas.jpg';
+import image24 from '../assets/images/3 Colunas de Texto e Imagem.jpg';
+import image25 from '../assets/images/Itens em 4 colunas.jpg';
+import image26 from '../assets/images/Itens com 3 imagens.jpg';
+import image27 from '../assets/images/Itens com 4 imagens.jpg';
+import image28 from '../assets/images/Depoimento.jpg';
+import image29 from '../assets/images/Itens em Lista.jpg';
+import image30 from '../assets/images/Faixa Ícone + Texto A.jpg';
+import image31 from '../assets/images/Faixa Ícone + Texto B.jpg';
+import image32 from '../assets/images/Faixa Cupom.jpg';
+import image33 from '../assets/images/Vitrine Individual.jpg';
+import image34 from '../assets/images/Vitrine Individual Invertida.jpg';
+import image35 from '../assets/images/Vitrine Individual Horizontal A.jpg';
+import image36 from '../assets/images/Vitrine Individual Horizontal B.jpg';
+import image37 from '../assets/images/Vitrine Dupla.jpg';
+import image38 from '../assets/images/Vitrine Dupla Simples.jpg';
+import image39 from '../assets/images/Vitrine Dupla Horizontal.jpg';
+import image40 from '../assets/images/Vitrine Dupla Horizontal Simples.jpg';
+import image41 from '../assets/images/Vitrine Tripla.jpg';
+import image42 from '../assets/images/Vitrine Tripla Simples.jpg';
+import image43 from '../assets/images/Vitrine Quádrupla.jpg';
+import image44 from '../assets/images/Vitrine Quádrupla Simples.jpg';
+
+const images = [
+  image1, image2, image3, image4, image5, image6, image7, image8, image9, image10,
+  image11, image12, image13, image14, image15, image16, image17, image18, image19, image20,
+  image21, image22, image23, image24, image25, image26, image27, image28, image29, image30,
+  image31, image32, image33, image34, image35, image36, image37, image38, image39, image40,
+  image41, image42, image43, image44,
+];
+
+export default images;
