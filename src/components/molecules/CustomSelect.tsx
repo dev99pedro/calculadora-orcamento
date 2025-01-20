db542baecb7a8251ae5e5189c1dd5f95a68ac461
PMWEB\evandro.guimaraes <evandro.guimaraes@pmweb.com.br>
2025-01-20 17:16:33 -0300

    WIP filter

--- src/components/molecules/CustomSelect.tsx
+++ src/components/molecules/CustomSelect.tsx
@@ -1,65 +1,41 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import EComplexity from '../../enums/EComplexity';
+import { ReactComponent as ToggleFilter } from '../../assets/svgs/toggleFilter.svg';
+import { ReactComponent as Checked } from '../../assets/svgs/checked.svg';
+import { ReactComponent as Unchecked } from '../../assets/svgs/unchecked.svg';
 
 const StyledSelect = styled.div`
-  width: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 5px;
-  border: 2px solid red;
-  height: 50px;
-  .select-selected {
-    border: 2px solid red;
-    position: relative;
-    background-color: blue;
-    width: 100%;
-    height: 5.2vh;
+  .select-items {
+    color: red;
+    cursor: pointer;
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    color: yellow;
-    padding: 8px 16px;
-    cursor: pointer;
-    border-radius: 8px;
-  }
-  .select-selected.select-arrow-active {
-    width: fit-content;
-    padding-right: 30px;
-  }
-  .select-selected:after {
-    position: absolute;
-    content: '';
-    top: calc(50% - 3px);
-    right: 10px;
-    width: 0;
-    height: 0;
-    border: 6px solid transparent;
-    border-color: red transparent transparent transparent;
-  }
-  .select-items div {
-    color: red;
-    padding: 8px 16px;
-    background-color: purple;
-    cursor: pointer;
-    width: 100%;
-    display: flex;
     align-items: flex-start;
     border-radius: 8px;
     z-index: 99999;
   }
-  .select-items div:hover {
-    background-color: rgba(138, 2, 202, 0.4);
-  }
-  .select-items {
-    border: 2px solid orange;
-    width: 100%;
-    border-radius: 8px;
-  }
   .select-hide {
     display: none;
+  }
+`;
+
+const StyledSelectTitle = styled.div<{ isOpen: boolean }>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+  background-color: #EBEBEB;
+  width: 124px;
+  height: 40px;
+  display: flex;
+  justify-content: space-evenly;
+  color: #666666;
+  cursor: pointer;
+  border-radius: 8px;
+  font-size: 12px;
+  svg{
+    transform: ${(props) => (props.isOpen ? 'rotate3d(1, 0, 0, 180deg)' : 'none')};
   }
 `;
 
@@ -89,28 +65,36 @@ function InputSelect(): JSX.Element {
 
   return (
     <StyledSelect className="custom-select">
-      <div
+      <StyledSelectTitle
+        isOpen={isOpen}
         className="select-selected"
         onClick={toggleDropdown}
         onKeyDown={(e) => e.key === 'Enter' && toggleDropdown()}
         role="button"
         tabIndex={0}
       >
-        {filtersCount ? `${filtersCount} complexidades` : 'Complexidade'}
-      </div>
+        {filtersCount ? `${filtersCount} complexidade${filtersCount > 1 ? 's' : ''}` : 'Complexidade'}
+        <ToggleFilter />
+      </StyledSelectTitle>
       <div className={`select-items ${isOpen ? '' : 'select-hide'}`}>
         {options.map((option) => (
-          <button
-            key={option}
-            onClick={() => selectOption(option)}
-            onKeyDown={(e) => e.key === 'Enter' && selectOption(option)}
-            role="option"
-            tabIndex={0}
-            aria-selected={filters.includes(option)}
-            type="button"
-          >
-            {option}
-          </button>
+          <>
+            {
+              filters.some((filter) => filter === option) ? <Checked /> : <Unchecked />
+            }
+            <div
+              key={option}
+              onClick={() => {
+                selectOption(option);
+              }}
+              onKeyDown={(e) => e.key === 'Enter' && selectOption(option)}
+              role="option"
+              tabIndex={0}
+              aria-selected={filters.includes(option)}
+            >
+              {option}
+            </div>
+          </>
         ))}
       </div>
     </StyledSelect>
